827bbf93c8a7fbb0b2cde77881c44d53932aafea
Chandan Shaw <chandan.shaw2050@gmail.com>
2022-04-02 18:50:47 +0530

    Aler Models Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertModalComponent } from './alert-modal/alert-modal.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Alerts';
+
+  constructor(public alertDialogBox: MatDialog) { }
+  ngOnInit(): void {
+
+  }
+
+
+  public openAlertDialogBox() {
+
+    this.alertDialogBox.open(AlertModalComponent, {
+      panelClass: 'custom-dialog-container',
+      height: '500px',
+      width: '1100px',
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlertModalComponent } from './alert-modal/alert-modal.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
